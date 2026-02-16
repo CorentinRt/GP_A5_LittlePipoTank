a325fb4b386a689a9416d1acca2057d5b6a0ce13
Haiito92 <hannaantoinebusiness@gmail.com>
2026-02-16 10:52:38 +0100

    [GP][ADD] NetworkHelpers BlueprintFunctionLibrary

---
+++ Source/GP_A5_LittlePipoTank/Public/Shared/NetworkProtocolHelpers.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "NetworkProtocolHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP_A5_LITTLEPIPOTANK_API UNetworkProtocolHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Serialization")
+	void SerializeI32(TArray<UINT8>& ByteArray, INT32 value);
+};
