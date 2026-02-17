809d45bf4dd46a4bd8569cde9797874e380969fd
Corentinrmt <corentin.remot@gmail.com>
2026-02-17 15:57:10 +0100

    [ADD] continue adding serialization packet for server opCodes

--- Source/GP_A5_LittlePipoTank/Private/Shared/NetworkProtocol.cpp
+++ Source/GP_A5_LittlePipoTank/Private/Shared/NetworkProtocol.cpp
@@ -37,14 +37,21 @@ void FPlayerLeftPacket::Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>:
 
 void FOwnPlayerStatePacket::Serialize(TArray<BYTE>& ByteArray) const
 {
+	// Size array
 	UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, PlayersData.Num());
 
 	for (const PlayerData& LocalPlayerData : PlayersData)
 	{
-		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.PlayerIndex);
-		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.PlayerLocation.X);
-		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.PlayerLocation.Y);
-		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.PlayerRotation);
+		// Index
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Index);
+		// Location
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Location.X);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Location.Y);
+		// Rotation
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Rotation);
+		// Velocity
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Velocity.X);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerData.Velocity.Y);
 	}
 }
 
@@ -62,13 +69,103 @@ void FOwnPlayerStatePacket::Deserialize(const TArray<BYTE>& ByteArray, TArray<BY
 
 		float PlayerRotation = UNetworkProtocolHelpers::DeserializeArithmetic<float>(ByteArray, Offset);
 		
+		FVector2D PlayerVelocity = FVector2D::ZeroVector;
+		PlayerVelocity.X = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+		PlayerVelocity.Y = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+		
 		PlayerData NewPlayerData
+		{
+			.Index = PlayerIndex,
+			.Location = PlayerLocation,
+			.Rotation = PlayerRotation,
+			.Velocity = PlayerVelocity
+		};
+		
+		PlayersData.Add(MoveTemp(NewPlayerData));
+	}
+}
+
+void FPlayersVisualPacket::Serialize(TArray<BYTE>& ByteArray) const
+{
+	// Size Array
+	UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, PlayersVisualData.Num());
+
+	for (const PlayerVisualData& LocalPlayerVisualData : PlayersVisualData)
+	{
+		// Index
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerVisualData.PlayerIndex);
+		// Location
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerVisualData.PlayerLocation.X);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerVisualData.PlayerLocation.Y);
+		// Rotation
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, LocalPlayerVisualData.PlayerRotation);
+	}
+}
+
+void FPlayersVisualPacket::Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset)
+{
+	TArray<BYTE>::SizeType PlayersDataSize = UNetworkProtocolHelpers::DeserializeArithmetic<TArray<BYTE>::SizeType>(ByteArray, Offset);
+
+	for (int i = 0; i < PlayersDataSize; ++i)
+	{
+		int PlayerIndex = UNetworkProtocolHelpers::DeserializeArithmetic<int>(ByteArray, Offset);
+
+		FVector2D PlayerLocation = FVector2D::ZeroVector;
+		PlayerLocation.X = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+		PlayerLocation.Y = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+
+		float PlayerRotation = UNetworkProtocolHelpers::DeserializeArithmetic<float>(ByteArray, Offset);
+		
+		PlayerVisualData NewPlayerVisualData
 		{
 			.PlayerIndex = PlayerIndex,
 			.PlayerLocation = PlayerLocation,
 			.PlayerRotation = PlayerRotation
 		};
 		
-		PlayersData.Add(MoveTemp(NewPlayerData));
+		PlayersVisualData.Add(MoveTemp(NewPlayerVisualData));
 	}
+}
+
+void FBulletsStatePacket::Serialize(TArray<BYTE>& ByteArray) const
+{
+	// Size Array
+	UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, BulletsStateData.Num());
+
+	for (const BulletStateData& BulletStateData : BulletsStateData)
+	{
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, BulletStateData.Location.X);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, BulletStateData.Location.Y);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, BulletStateData.Rotation);
+		UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, BulletStateData.Destroyed);
+	}
+
+	
+}
+
+void FBulletsStatePacket::Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset)
+{
+	TArray<BYTE>::SizeType BulletsDataSize = UNetworkProtocolHelpers::DeserializeArithmetic<TArray<BYTE>::SizeType>(ByteArray, Offset);
+
+	for (int i = 0; i < BulletsDataSize; ++i)
+	{
+		FVector2D BulletLocation = FVector2D::ZeroVector;
+		BulletLocation.X = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+		BulletLocation.Y = UNetworkProtocolHelpers::DeserializeArithmetic<double>(ByteArray, Offset);
+		
+		float BulletRotation = UNetworkProtocolHelpers::DeserializeArithmetic<float>(ByteArray, Offset);
+		bool BulletDestroyed = UNetworkProtocolHelpers::DeserializeArithmetic<bool>(ByteArray, Offset);
+	}
+}
+
+void FGamePhasePacket::Serialize(TArray<BYTE>& ByteArray) const
+{
+	uint8 GamePhaseByte = static_cast<uint8>(GamePhase);
+	UNetworkProtocolHelpers::SerializeArithmetic(ByteArray, GamePhaseByte);
+}
+
+void FGamePhasePacket::Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset)
+{
+	uint8 GamePhaseByte = UNetworkProtocolHelpers::DeserializeArithmetic<uint8>(ByteArray, Offset);
+	GamePhase = static_cast<ETankGamePhase>(GamePhaseByte);
 }
--- Source/GP_A5_LittlePipoTank/Public/Shared/NetworkProtocol.h
+++ Source/GP_A5_LittlePipoTank/Public/Shared/NetworkProtocol.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankGamePhase.h"
 #include "UObject/Object.h"
 #include "NetworkProtocol.generated.h"
 
@@ -17,7 +18,7 @@ enum class OpCode : UINT8
 	S_PlayerJoined = 1,
 	S_PlayerLeft = 2,
 	S_OwnPlayerState = 3,
-	S_PlayerVisual = 4,
+	S_PlayersVisual = 4,
 	S_BulletsState = 5,
 	S_GamePhase = 6,
 
@@ -83,13 +84,74 @@ struct FOwnPlayerStatePacket
 	
 	struct PlayerData
 	{
+		int Index = -1;
+		FVector2D Location = FVector2D::ZeroVector;
+		float Rotation = 0.f;
+		float AimRotation = 0.f;
+		FVector2D Velocity = FVector2D::ZeroVector;
+	};
+
+	TArray<PlayerData> PlayersData;
+
+	void Serialize(TArray<BYTE>& ByteArray) const;
+	void Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset);
+};
+
+USTRUCT()
+struct FPlayersVisualPacket
+{
+	GENERATED_BODY()
+
+	~FPlayersVisualPacket() = default;
+
+	static constexpr OpCode OpCode = OpCode::S_PlayersVisual;
+
+	struct PlayerVisualData
+	{
 		int PlayerIndex = -1;
 		FVector2D PlayerLocation = FVector2D::ZeroVector;
 		float PlayerRotation = 0.f;
 		float AimRotation = 0.f;
 	};
 
-	TArray<PlayerData> PlayersData;
+	TArray<PlayerVisualData> PlayersVisualData;
+
+	void Serialize(TArray<BYTE>& ByteArray) const;
+	void Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset);
+};
+
+USTRUCT()
+struct FBulletsStatePacket
+{
+	GENERATED_BODY()
+
+	~FBulletsStatePacket() = default;
+
+	static constexpr OpCode OpCode = OpCode::S_BulletsState;
+	
+	struct BulletStateData
+	{
+		FVector2D Location = FVector2D::ZeroVector;
+		float Rotation = 0.f;
+		bool Destroyed = false;
+	};
+
+	TArray<BulletStateData> BulletsStateData;
+
+	void Serialize(TArray<BYTE>& ByteArray) const;
+	void Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset);
+};
+
+USTRUCT()
+struct FGamePhasePacket
+{
+	GENERATED_BODY()
+
+	~FGamePhasePacket() = default;
+
+	static constexpr OpCode OpCode = OpCode::S_GamePhase;
+
+	ETankGamePhase GamePhase = ETankGamePhase::NONE;
 
 	void Serialize(TArray<BYTE>& ByteArray) const;
 	void Deserialize(const TArray<BYTE>& ByteArray, TArray<BYTE>::SizeType& Offset);
